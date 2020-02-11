ddab4158c05c16a6be05dc961586131013d75e13
James <IEatPie@github.com>
2020-02-10 22:16:26 -0800

    implement Grub, Bee, HoneyMakerBee, ForagerBee, and RetiredForagerBee

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,15 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function(food){
+  if (food === this.food) {
+    console.log('Yum!');
+    return true;
+  } else {
+    console.log('I can\'t eat this :(');
+    return false;
+  }
+};
